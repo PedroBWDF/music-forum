ec96464142703314a65be5ad669a9cc20a3b39cc
PedroBWDF <bwdf64587@gmail.com>
2024-06-04 18:11:36 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const routes = require('./routes')
 
 const path = require('path')
 const handlebars = require('express-handlebars')
+
+const session = require('express-session')
+const flash = require('connect-flash')
+const SESSION_SECRET = 'secret'
+
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
@@ -17,6 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // const db = require('./models') 測試與資料庫連線用
 
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const musicController = require('../controllers/music-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 // 引入admin
 const admin = require('./modules/admin')
@@ -13,6 +14,8 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+router.use('/', generalErrorHandler)
 
 // router.use為路由加上middleware。若上面路由都未符合，就redirect到/music
 router.use('/', (req, res) => {
